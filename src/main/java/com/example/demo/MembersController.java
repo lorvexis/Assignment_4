cc6bc377e93ce84f6bf90965e3bd553d836fef10
Islam <kulanbaevi99@gmail.com>
2026-01-26 14:14:24 +0500

    new2

---
+++ src/main/java/com/example/demo/MembersController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/members")
+public class MembersController {
+
+    private final Library library = new Library();
+
+    @GetMapping
+    public List<LibraryMember> getMembers() {
+        return library.getAllMembers();
+    }
+
+    @PostMapping
+    public String addMember(@RequestBody LibraryMember member) {
+        library.AddMemberToDatabase(member);
+        return "Member added";
+    }
+}
